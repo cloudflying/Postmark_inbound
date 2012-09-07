5dc96e7b8e5a2e8a18b539f20118f4616473ba3f
Justin <justin@textconnects.com>
2012-09-07 14:31:55 -0400

    initial commit

---
+++ postmark_inbound.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json
+
+Public Class postmark_inbound
+
+    Public Function ParseInbound(emailContent As Stream) As mail_object
+        Try
+            Dim reader As New System.IO.StreamReader(emailContent)
+            Dim storeEMail As String = reader.ReadToEnd
+            Dim Email As mail_object = JsonConvert.DeserializeObject(Of mail_object)(storeEMail)
+            Return Email
+        Catch ex As Exception
+            Throw New ApplicationException(ex.Message)
+        End Try
+    End Function
+
+
+
+End Class
---
+++ properties/mail_object.vb
@@ -0,0 +1,47 @@
+﻿Imports Newtonsoft.Json
+Public Class mail_object
+    Public Property From As String
+    Public Property FromFull As FromFull
+    <JsonProperty("To")> _
+    Public Property [To] As String
+    Public Property ToFull As List(Of ToFull)
+    Public Property Cc As String
+    Public Property CcFull As List(Of CcFull)
+    Public Property ReplyTo As String
+    Public Property Subject As String
+    Public Property MessageID As String
+    <JsonProperty("Date")> _
+    Public Property [Date] As String
+    Public Property MailboxHash As String
+    Public Property TextBody As String
+    Public Property HtmlBody As String
+    Public Property Tag As String
+    Public Property Headers As List(Of Header)
+    Public Property Attachments As List(Of Attachment)
+End Class
+
+Public Class FromFull
+    Public Property email As String
+    Public Property name As String
+End Class
+
+Public Class ToFull
+    Public Property Email As String
+    Public Property Name As String
+End Class
+
+Public Class CcFull
+    Public Property Email As String
+    Public Property Name As String
+End Class
+
+Public Class Header
+    Public Property Name As String
+    Public Property Value As String
+End Class
+Public Class Attachment
+    Public Property Name As String
+    Public Property Content As String
+    Public Property ContentType As String
+    Public Property ContentLength As Integer
+End Class
